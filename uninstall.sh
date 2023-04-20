f040e41a50eb1475b76067afce059c3228257e7b
GiuCoder <128739221+GiuCoder@users.noreply.github.com>
2023-04-20 10:49:44 +0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
+# Define color variables
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
 # Check for root privileges
 if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root."
+    echo -e "${RED}This script must be run as root.${NC}"
     exit 1
 fi
 
 # Check if hidemyip.sh is installed
-if [ ! -f "/usr/bin/hidemyip.sh" ]; then
-    echo "hidemyip.sh is not installed."
+if ! command -v hidemyip.sh &> /dev/null; then
+    echo -e "${RED}hidemyip.sh is not installed.${NC}"
     exit 1
 fi
 
-# Remove hidemyip.sh
-sudo rm /usr/bin/hidemyip.sh
+# Show loading animation while deleting hidemyip.sh
+echo -e "${YELLOW}Deleting hidemyip.sh...${NC}"
+spinner="/-\|"
+while sudo rm -f /usr/bin/hidemyip.sh 2>/dev/null; do
+    printf "\b${spinner:0:1}"
+    spinner=${spinner:1}${spinner:0:1}
+done
+echo -e "\n${GREEN}hidemyip.sh has been deleted successfully.${NC}"
 
-echo "hidemyip.sh has been successfully deleted."
+echo -e "${YELLOW}Removing HideYourSelf directory...${NC}"
+rm -rf HideYourSelf
+echo -e "${GREEN}HideYourSelf directory has been removed.${NC}"
 
-# Check if HideYourSelf directory exists
-if [ ! -d "HideYourSelf" ]; then
-    echo "HideYourSelf directory is not found."
-    exit 1
-fi
-
-# Remove HideYourSelf directory
-rm -r HideYourSelf
-
-echo "HideYourSelf directory has been successfully deleted."
-
-echo "To use this program again, follow the instructions on GitHub." 
+echo -e "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+echo -e "${GREEN}Created By GiuCoder${NC}"
