7fafc8e28611655b0ca28413bd85d20d8abbe0be
GiuCoder <128739221+GiuCoder@users.noreply.github.com>
2023-04-20 11:01:06 +0700

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -8,24 +8,24 @@ NC='\033[0m'
 
 # Check for root privileges
 if [[ $EUID -ne 0 ]]; then
-    echo -e "${RED}This script must be run as root.${NC}"
+    echo -e "${RED}This script must be run as root.${NC}\n"
     exit 1
 fi
 
 # Check if hidemyip.sh is installed
 if ! command -v hidemyip.sh &> /dev/null; then
-    echo -e "${RED}hidemyip.sh is not installed.${NC}"
+    echo -e "${RED}hidemyip.sh is not installed.${NC}\n"
     exit 1
 fi
 
 # Delete hidemyip.sh
 sudo rm -f /usr/bin/hidemyip.sh
-echo -e "${GREEN}hidemyip.sh has been deleted successfully.${NC}"
+echo -e "${GREEN}hidemyip.sh has been deleted successfully.${NC}\n"
 
 # Remove HideYourSelf directory
-echo -e "${YELLOW}Removing HideYourSelf directory...${NC}"
+echo -e "${YELLOW}Removing HideYourSelf directory...${NC}\n"
 rm -rf HideYourSelf
-echo -e "${GREEN}HideYourSelf directory has been removed.${NC}"
+echo -e "${GREEN}HideYourSelf directory has been removed.${NC}\n"
 
 # Print message for how to remove all the program
 echo -e "${YELLOW}To Remove All The Program Please Run:${NC}\n"
