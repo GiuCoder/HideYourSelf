2d823258abdb0d12652d3b69d9d7d17e87b81b3d
GiuCoder <128739221+GiuCoder@users.noreply.github.com>
2023-04-27 23:00:57 +0700

    Update hidemyip.sh

--- hidemyip.sh
+++ hidemyip.sh
@@ -5,15 +5,21 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+# Check if the user is running a supported Linux distribution
+if [[ "$(cat /etc/os-release | grep -o '^ID=.*' | cut -d '=' -f 2)" != "ubuntu" ]]; then
+    printf "${RED}This script is only supported on Ubuntu.${NC}\n"
+    exit 1
+fi
+
 # Check if zenity is installed
 if ! command -v zenity &> /dev/null; then
-    echo -e "${RED}Zenity is not installed. Installing...${NC}"
-    sudo apt-get install zenity
+    printf "${RED}Zenity is not installed. Installing...${NC}\n"
+    sudo apt-get install zenity || { printf "${RED}Failed to install Zenity.${NC}\n"; exit 1; }
 fi
 
 # Check for root privileges
 if [[ $EUID -ne 0 ]]; then
-    echo -e "${RED}This script must be run as root.${NC}"
+    printf "${RED}This script must be run as root.${NC}\n"
     exit 1
 fi
 
@@ -22,16 +28,16 @@ ip_addr=$(zenity --entry --title="Enter your IP address" --text="Please enter yo
 
 # Check if the user entered an IP address
 if [ -z "$ip_addr" ]; then
-    echo -e "${RED}No IP address entered.${NC}"
+    printf "${RED}No IP address entered.${NC}\n"
     exit 1
 fi
 
 # Add the iptables rule to drop incoming traffic from the specified IP address
 if sudo iptables -A INPUT -s "$ip_addr" -j DROP; then
-    echo -e "${GREEN}Success! Your IP address has been hidden from the Internet.${NC}"
+    printf "${GREEN}Success! Your IP address has been hidden from the Internet.${NC}\n"
     zenity --info --title "Success" --text "Your IP address has been hidden from the Internet."
 else
-    echo -e "${RED}An error occurred while hiding your IP address.${NC}"
+    printf "${RED}An error occurred while hiding your IP address.${NC}\n"
     zenity --error --title "Error" --text "An error occurred while hiding your IP address."
 fi
 
